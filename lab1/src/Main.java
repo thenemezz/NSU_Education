f31bf90d3a9cd82611da99facfca060aeaeb6451
Сергей Карпиевич <thenemez@MacBook-Pro-Sergej.local>
2024-02-02 23:18:04 +0700

    Without stringBuilder

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -3,10 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
-//import stringBuilder.java
 import java.util.LinkedHashMap;
 import java.util.stream.Collectors;
 import java.io.BufferedWriter;
@@ -16,23 +13,21 @@ import java.io.FileWriter;
 public class Main {
     public static void main(String[] args) {
         int totalFrequency = 0;
-        String tmpStr = args[0];
+        String fileName = args[0];
         Reader reader = null;
         try {
-            reader = new InputStreamReader(new FileInputStream(tmpStr + ".txt"));
-            String tmp_str = "";
+            reader = new InputStreamReader(new FileInputStream(fileName + ".txt"));
+            String currentWord = "";
             int currentChar;
             Map<String, Integer> wordCounterMap = new HashMap<>();
+
             while ((currentChar = reader.read()) != -1) {
-
-                char letter = (char) currentChar;
-
-                if (Character.isLetterOrDigit(letter)) {
-                     tmp_str = tmp_str + letter;
+                if (Character.isLetterOrDigit((char) currentChar)) {
+                     currentWord += (char) currentChar;
                 }
                 else {
-                    wordCounterMap.merge(tmp_str, 1, Integer::sum);
-                    tmp_str = "";
+                    wordCounterMap.merge(currentWord, 1, Integer::sum);
+                    currentWord = "";
                     ++totalFrequency;
                 }
             }
@@ -46,25 +41,20 @@ public class Main {
                             (e1, e2) -> e1,
                             LinkedHashMap::new
                     ));
-            String csvFilePath = "test.csv";
+
+            String csvFilePath = "output.csv";
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath))) {
-                writer.write("Word,frequency,frequency(%)\n");
+                writer.write("Word,Frequency,Frequency(%)\n");
 
                 // Записываем данные из хеш-таблицы
                 for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
-                    double percentage = ((double) entry.getValue() / totalFrequency) * 100.0;
-                    //String formattedPercentage = String.format("%.2f", percentage);
-                    writer.write(entry.getKey() + "," + entry.getValue() + "," + String.format("%.0f", percentage) + "\n");
+                    writer.write(entry.getKey() + "," + entry.getValue() + "," + String.format("%.0f", ((double) entry.getValue() / totalFrequency) * 100.0) + "\n");
                 }
+                System.out.println("The data has been successfully written to a CSV file.");
 
-                //writer.write(entry.getKey() + "," + entry.getValue() + "," + + ""\n");
-                System.out.println("Данные успешно записаны в CSV файл.");
             } catch (IOException e) {
                 e.printStackTrace();
             }
-//            wordCounterMap.entrySet().stream()
-//                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-//                    .forEach(System.out::println);
 
 
         }
@@ -91,6 +81,11 @@ public class Main {
 
     }
 }
+
+//сортировка мапы другая
+//            wordCounterMap.entrySet().stream()
+//                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+//                    .forEach(System.out::println);
 
 //вывод отсортированной мапы
 //            for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
