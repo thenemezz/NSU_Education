654cef40ff03bbef1ee438c525edf2fc13f430c8
thenemez <thenemezz@gmail.com>
2024-10-12 01:09:51 +0700

    feat: add config

---
+++ networks/thirdTask/config.py
@@ -0,0 +1,9 @@
+# API ключи и URL
+GRAPHOPPER_API_KEY = '74e86a83-1a91-4f80-9291-f56a2f0378b3'
+OPENWEATHERMAP_API_KEY = 'd29eab97c4d69a6c51f3ee485a6d0662'
+OPENTRIPMAP_API_KEY = '5ae2e3f221c38a28845f05b6ed5af70c89b75a6de9898e0c5c4fd2af'
+
+GRAPHOPPER_GEOCODE_URL = 'https://graphhopper.com/api/1/geocode?q=%s&locale=ru&key=%s'
+OPENWEATHERMAP_URL = 'https://api.openweathermap.org/data/2.5/weather'
+OPENTRIPMAP_PLACES_URL = 'https://api.opentripmap.com/0.1/en/places/radius?radius=%d&lon=%f&lat=%f&kinds=interesting_places&limit=%d&format=geojson&apikey=%s'
+OPENTRIPMAP_PLACE_DETAILS_URL = 'https://api.opentripmap.com/0.1/en/places/xid/%s?apikey=%s'
